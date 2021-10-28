58d73b8f742731a1ee6be843429c5bb5b815bae8
tjorge30 <mrs.javalera@gmail.com>
2021-10-28 13:44:24 -0600

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Tasha Javalera on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
